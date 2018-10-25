ddcfdb073cf6691030c08dc018b8a4d93c6e9723
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-10-25 11:19:13 -0600

    added cors, heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 let port = process.env.PORT || 3000;
 
 //Route imports
